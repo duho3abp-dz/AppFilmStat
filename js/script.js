7f9f44358d4bfbd1d62f8a87f02a8e9869752478
duho3abp <DanZhidkof@yandex.ru>
2020-06-12 02:09:22 +0300

    Add conditions and cycles

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 'use strict';
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
+while (numberOfFilms < 1 || numberOfFilms < '' || numberOfFilms.length > 50) {
+    numberOfFilms = 0;
+
+    if (numberOfFilms !=  0){
+        break;
+    } else {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -11,13 +20,104 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = +prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = +prompt('На сколько оцените его?', '');
+for (let i = 0; i < 2; i++){
+    switch (i) {
+        case 0:
+            let a = prompt('Один из последних просмотренных фильмов?', ''),
+                b = +prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+            while (a < 1 || a < '' || a.length > 50) {
+                    a = 0;
 
+                    if (a !=  0){
+                        break;
+                    } else {
+                        a = prompt('Один из последних просмотренных фильмов?', '');
+                    }
+            }
+            while (b < 1 || b < '' || b.length > 50) {
+                    b = 0;
+
+                    if (b !=  0){
+                        break;
+                    } else {
+                        b = +prompt('На сколько оцените его?', '');
+                    }
+            }
+
+            personalMovieDB.movies[a] = b;
+            break;
+
+        case 1:
+            let c = prompt('Один из последних просмотренных фильмов?', ''),
+                d = +prompt('На сколько оцените его?', '');
+
+            while (c < 1 || c < '' || c.length > 50) {
+                    c = 0;
+
+                    if (c !=  0){
+                        break;
+                    } else {
+                        c = prompt('Один из последних просмотренных фильмов?', '');
+                    }
+            }
+            while (d < 1 || d < '' || d.length > 50) {
+                    d = 0;
+
+                    if (d !=  0){
+                        break;
+                    } else {
+                        d = +prompt('На сколько оцените его?', '');
+                    }
+            }
+
+            personalMovieDB.movies[c] = d;
+            break;
+    
+        default:
+            break;
+    }
+}
 
 console.log(personalMovieDB);
+
+// Нельзя оставить ответ в виде пустой строки, его отменить, а так же ввести больше 50 символов!
+// while (numberOfFilms < 1 || numberOfFilms < '' || numberOfFilms.length > 50) {
+//     numberOfFilms = 0;
+
+//     if (numberOfFilms !=  0){
+//         break;
+//     } else {
+//         numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+//     }
+// }
+
+// for (let i = 0; i <= 1; i++) {
+//     personalMovieDB.movies[prompt('Один из последних просмотренных фильмов?', '')] = +prompt('На сколько оцените его?', '');
+// }
+
+
+
+//------------------------ferst--------------
+
+
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+//       b = +prompt('На сколько оцените его?', ''),
+//       c = prompt('Один из последних просмотренных фильмов?', ''),
+//       d = +prompt('На сколько оцените его?', '');
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+// console.log(personalMovieDB);
